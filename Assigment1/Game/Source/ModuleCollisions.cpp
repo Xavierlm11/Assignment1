e05703d4e8d7d9d7962ef8177d03126ffdf736ac
Xavierlm11 <xaviloma11@gmail.com>
2021-11-07 01:39:25 +0100

    Update ModuleCollisions.cpp

--- Assigment1/Game/Source/ModuleCollisions.cpp
+++ Assigment1/Game/Source/ModuleCollisions.cpp
@@ -71,68 +71,6 @@ ModuleCollisions::~ModuleCollisions()
 
 }
 
-//bool ModuleCollisions::PreUpdate()
-//{
-//	// Remove all colliders scheduled for deletion
-//	for(uint i = 0; i < MAX_COLLIDERS; ++i)
-//	{
-//		if(colliders[i] != nullptr && colliders[i]->pendingToDelete == true)
-//		{
-//			delete colliders[i];
-//			colliders[i] = nullptr;
-//		}
-//	}
-//
-//	Collider* c1;
-//	Collider* c2;
-//
-//	for(uint i = 0; i < MAX_COLLIDERS; ++i)
-//	{
-//		// skip empty colliders
-//		if(colliders[i] == nullptr)
-//			continue;
-//
-//		c1 = colliders[i];
-//
-//		// avoid checking collisions already checked
-//		for(uint k = i+1; k < MAX_COLLIDERS; ++k)
-//		{
-//			// skip empty colliders
-//			if(colliders[k] == nullptr)
-//				continue;
-//
-//			c2 = colliders[k];
-//
-//			//if(c1->Intersects(c2->rect))
-//			//{
-//			//	if(matrix[c1->type][c2->type] && c1->listener) 
-//			//		c1->listener->OnCollision(c1, c2);
-//			//	
-//			//	if(matrix[c2->type][c1->type] && c2->listener) 
-//			//		c2->listener->OnCollision(c2, c1);
-//			//}
-//
-//			if (matrix[c1->type][c2->type] && c1->Intersects(c2->rect))
-//			{
-//				for (uint i = 0; i < MAX_LISTENERS; ++i)
-//				{
-//					if (c1->listeners[i] != nullptr)
-//					{
-//						c1->listeners[i]->OnCollision(c1, c2);
-//					}
-//				}
-//				for (uint i = 0; i < MAX_LISTENERS; ++i)
-//				{
-//					if (c2->listeners[i] != nullptr) {
-//						c2->listeners[i]->OnCollision(c2, c1);
-//					}
-//				}
-//			}
-//		}
-//	}
-//
-//	return true;
-//}
 bool ModuleCollisions::PreUpdate() {
 	for (uint i = 0; i < MAX_COLLIDERS; i++) {
 		if (colliders[i] != nullptr && colliders[i]->pendingToDelete == true) {
@@ -176,52 +114,6 @@ bool ModuleCollisions::PreUpdate() {
 	return true;
 }
 
-//bool ModuleCollisions::PreUpdate()
-//{
-//	// Remove all colliders scheduled for deletion
-//	for (uint i = 0; i < MAX_COLLIDERS; ++i)
-//	{
-//		if (colliders[i] != nullptr && colliders[i]->pendingToDelete == true)
-//		{
-//			delete colliders[i];
-//			colliders[i] = nullptr;
-//		}
-//	}
-//
-//	Collider* c1;
-//	Collider* c2;
-//
-//	for (uint i = 0; i < MAX_COLLIDERS; ++i)
-//	{
-//		// skip empty colliders
-//		if (colliders[i] == nullptr)
-//			continue;
-//
-//		c1 = colliders[i];
-//
-//		// avoid checking collisions already checked
-//		for (uint k = i + 1; k < MAX_COLLIDERS; ++k)
-//		{
-//			// skip empty colliders
-//			if (colliders[k] == nullptr)
-//				continue;
-//
-//			c2 = colliders[k];
-//
-//			if (c1->Intersects(c2->rect))
-//			{
-//				if (matrix[c1->type][c2->type] && c1->listener)
-//					c1->listener->OnCollision(c1, c2);
-//
-//				if (matrix[c2->type][c1->type] && c2->listener)
-//					c2->listener->OnCollision(c2, c1);
-//			}
-//		}
-//	}
-//
-//	return true;
-//}
-
 bool ModuleCollisions::Update(float dt)
 {
 	// Get gamepad info
@@ -229,8 +121,6 @@ bool ModuleCollisions::Update(float dt)
 	if ((app->input->GetKey(SDL_SCANCODE_F9) == KEY_DOWN) ) {
 		debug = !debug;
 	}
-	
-
 
 	return true;
 }
