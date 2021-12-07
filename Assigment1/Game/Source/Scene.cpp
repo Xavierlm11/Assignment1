3cdc75d0408087b37781377cc2961d1dcc2abbff
Xavierlm11 <xaviloma11@gmail.com>
2021-12-07 20:14:57 +0100

    lvl1 update

--- Assigment1/Game/Source/Scene.cpp
+++ Assigment1/Game/Source/Scene.cpp
@@ -198,19 +198,7 @@ bool Scene::PreUpdate()
 
 	if (level1) {
 		app->map->Load("level1.tmx");
-		Check1 = app->coll->AddCollider({ 70, 260, 20,20 }, Collider::Type::CHECKPOINT1, this);
-		Check2 = app->coll->AddCollider({ 233, 26, 20,20 }, Collider::Type::CHECKPOINT2, this);
-		Check3 = app->coll->AddCollider({ 590, 220, 20,20 }, Collider::Type::CHECKPOINT3, this);
-
-		//Item Collider
-		Item1 = app->coll->AddCollider({ 460, 214, 14,14 }, Collider::Type::ITEM1, this);
-		Item2 = app->coll->AddCollider({ 200, 287, 14,14 }, Collider::Type::ITEM2, this);
-		KeyColl = app->coll->AddCollider({ 206, 140, 14,14 }, Collider::Type::KEY, this);
-
-		//Coin Collider
-		CoinColl1 = app->coll->AddCollider({ 18,171, 12,12 }, Collider::Type::COIN1, this);
-		CoinColl2 = app->coll->AddCollider({ 540, 135, 12,12 }, Collider::Type::COIN2, this);
-		CoinColl3 = app->coll->AddCollider({ 289, 175, 12,12 }, Collider::Type::COIN3, this);
+		StartColliders();
 		app->map->CreateColliders();
 
 		level1 = false;
