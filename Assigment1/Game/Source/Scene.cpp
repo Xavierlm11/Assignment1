3c51a3c001594cc8e000198125ca7eec4ef0df05
Xavierlm11 <xaviloma11@gmail.com>
2021-12-12 01:41:16 +0100

    Update Scene.cpp

--- Assigment1/Game/Source/Scene.cpp
+++ Assigment1/Game/Source/Scene.cpp
@@ -341,7 +341,7 @@ bool Scene::Update(float dt)
 			Level1ToLevel2();
 		}
 
-		if (app->input->GetKey(SDL_SCANCODE_F11) == KEY_DOWN) {
+		if (app->input->GetKey(SDL_SCANCODE_F12) == KEY_DOWN) {
 			win = true;
 			WinAnim.Reset();
 			currentScene = WIN_GAME;
@@ -393,7 +393,7 @@ bool Scene::Update(float dt)
 				ResetGame();
 			}
 
-			if (app->input->GetKey(SDL_SCANCODE_F11) == KEY_DOWN) {
+			if (app->input->GetKey(SDL_SCANCODE_F12) == KEY_DOWN) {
 				win = true;
 				WinAnim.Reset();
 				currentScene = WIN_GAME;
